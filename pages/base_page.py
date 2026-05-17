9ecda36f91be7070e1a417366b02995416e0fb92
AleksK <a.karagodin.001@gmail.com>
2026-05-17 20:08:37 +0300

    start project

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-
+import time
 
 class BasePage:
 
@@ -11,5 +11,8 @@ class BasePage:
         return self.driver.get(self.base_url)
 
     def find_element(self, locator):
+        time.sleep(3)
         return self.driver.find_element(By.CSS_SELECTOR, locator)
 
+def get_url(self):
+        return self.driver.current_url
