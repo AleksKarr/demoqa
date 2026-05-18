9bd89fa8c53411e0efb4086edc78027a7db2a1bc
AleksK <a.karagodin.001@gmail.com>
2026-05-18 09:27:01 +0300

    start project

--- pages/demoqa.py
+++ pages/demoqa.py
@@ -3,19 +3,27 @@ from pages.base_page import BasePage
 
 class DemoQa(BasePage):
 
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super().__init__(driver, self.base_url)
+
     def exist_icon(self):
         try:
-            self.find_element(locator='#app > header > a')
+            self.find_element(locator='header a')
         except NoSuchElementException:
             return False
         return True
 
     def click_on_the_icon(self):
-        self.find_element(locator='#app > header > a').click()
+        self.find_element(locator='header a').click()
+
+    def click_on_the_btn(self):
+            self.find_element(locator="div.card.mt-4.top-card").click()
 
     def equal_url(self):
-        if self.get_url() =='https://demoqa.com'
+        if self.get_url() == self.base_url:
             return True
         else:
             return False
+        pass
 
