9f499c07ce6a928437960b84d48b79c6554fd45c
AleksK <a.karagodin.001@gmail.com>
2026-05-19 12:55:43 +0300

    start project

--- components/components.py
+++ components/components.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 
 class WebElement:
     def __init__(self, driver, locator):
@@ -14,6 +16,12 @@ class WebElement:
     def get_text(self):
         return str(self.find_element().text)
 
+    def exist(self):
+        try:
+            self.find_element()
+        except NoSuchElementException:
+            return False
+        return True
 
 
 
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -8,6 +8,12 @@ class ElementsPage(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
-        self.text_center = WebElement(driver, "div.col-md-6")
+
+        self.text_please = WebElement(driver, '#app > div > div > div.row >div.col-12.mt-4.col-md-6')
+        self.icon = WebElement(driver, 'header > a > img')
+        self.text_center = WebElement(driver, "#root > div > div > div > div.col-12.mt-4.col-md-6.col-xl-7")
+        self.text_elements = WebElement(driver, 'div.col-12:nth-child(2)')
+        self.btn_sidebar_first = WebElement(driver,'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_textbox = WebElement(driver,'#item-0 > a')
 
 
