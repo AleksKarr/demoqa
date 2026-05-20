07e79b1299a8647aaf07760edff54706b99d1fc2
AleksK <a.karagodin.001@gmail.com>
2026-05-20 10:55:35 +0300

    start project

---
+++ tests/test_check_text.py
@@ -0,0 +1,30 @@
+from pages.demoqa import DemoQa
+
+
+def test_check_text(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    assert demo_qa_page.footer.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+
+from pages.demoqa import DemoQa
+from pages.elements_page import ElementsPage
+
+
+def test_go_to_page_element(browser):
+    demo_qa_page = DemoQa(browser)
+    elements_page = ElementsPage(browser)
+
+    demo_qa_page.visit()
+    demo_qa_page.btn_elements.click()
+
+    assert elements_page.text_center.get_text() == "Please select an item from left to start practice."
+
+def test_page_elements(browser):
+    elements_page = ElementsPage(browser)
+    elements_page.visit()
+
+    assert elements_page.text_elements.get_text() == 'Please select an item from left to start practice.'
+
+    assert elements_page.icon.exist()
+    assert elements_page.btn_sidebar_first.exist()
+    assert elements_page.btn_sidebar_first_textbox.exist()
