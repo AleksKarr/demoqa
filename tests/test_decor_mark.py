f59016001e246f53c13f16656c605d61d808b797
AleksK <a.karagodin.001@gmail.com>
2026-05-25 15:33:49 +0300

    start project

---
+++ tests/test_decor_mark.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.mark.smoke
+def test_mark_1(browser):
+    assert True
+
+
+@pytest.mark.regress
+def test_mark_2(browser):
+    assert True
+
+@pytest.mark.regress
+def test_mark_3(browser):
+    assert True
+
+
+@pytest.mark.regress
+def test_mark_4(browser):
+    assert True
